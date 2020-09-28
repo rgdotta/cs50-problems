bb700061f05a1eda8f398f5f3a9b0fe5e200b5ef
Rafael Gimenez Dotta <rafaelgdotta@gmail.com>
2020-09-28 15:03:22 -0300

    Update mario.js

--- Week 1/mario.js
+++ Week 1/mario.js
@@ -9,12 +9,16 @@ const times = x => f => {
   }
 }
 
-for(i=0; i<x + 1; i++){
-	times(x - i)(() => y = y + " ")
-  times(i)(() => y = y + "*")
-  y = y + " "
-  times(i)(() => y = y + "*")
-  y = y + "\n"
+if(typeof x === 'number'){
+  for(i=0; i<x + 1; i++){
+    times(x - i)(() => y = y + " ")
+    times(i)(() => y = y + "*")
+    y = y + " "
+    times(i)(() => y = y + "*")
+    y = y + "\n"
+  }
+} else { 
+   y = "Var x is not a number";
 }
 
 console.log(y)
